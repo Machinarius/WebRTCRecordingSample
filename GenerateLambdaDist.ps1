5c2140994534de499991085124236bf4a497b99d
Germán Valencia <german.valencia@salsamobi.com>
2020-04-27 13:16:19 -0500

    Lambda Dist script improvements

--- GenerateLambdaDist.ps1
+++ GenerateLambdaDist.ps1
@@ -1,5 +1,6 @@
 param (
-    [switch] $Deploy
+    [switch] $Deploy,
+    [switch] $RunLocal
 )
 
 If (Test-Path .\dist) {
@@ -16,12 +17,30 @@ Set-Location ..\server
 yarn run tsc --sourceMap false --outDir ..\dist\ --project .\tsconfig.json
 
 Set-Location ..
-Copy-Item -Recurse -Container .\server\node_modules .\dist\node_modules
+Copy-Item .\server\package.json .\dist\package.json
+Copy-Item .\server\yarn.lock .\dist\yarn.lock
+
+Set-Location .\dist
+yarn install --production
+Set-Location ..
+
+If (Test-Path .\server\.env) {
+    Copy-Item .\server\.env .\dist\.env
+}
 
 Copy-Item -Recurse -Container .\client\dist .\dist\frontend\dist
 Copy-Item .\client\index.html .\dist\frontend\index.html
 
 Copy-Item .\serverless.yml .\dist\serverless.yml
+
+If ($RunLocal) {
+    try {
+        Set-Location .\dist
+        node .\LocalEntrypoint.js
+    } finally {
+        Set-Location ..
+    }
+}
 
 If ($Deploy) {
     Set-Location .\dist
