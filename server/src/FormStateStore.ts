72325106cc9ae2257992b6e0a99d8c015fdd0ec3
Germán Valencia <german.valencia@salsamobi.com>
2020-06-08 19:02:45 -0500

    The form status is now written to S3

--- server/src/FormStateStore.ts
+++ server/src/FormStateStore.ts
@@ -1,17 +1,21 @@
-const MONGO_URL = process.env.MONGO_URL;
-if (!MONGO_URL) {
-    throw new Error("Please ensure that the MONGO_URL env var is defined");
+const TARGET_BUCKET = process.env.S3_TARGET_BUCKET;
+if (!TARGET_BUCKET) {
+    throw new Error("Make sure S3_TARGET_BUCKET is defined in the .env file, and that it points to a valid writeable folder");
 }
 
-const MONGO_DB = process.env.MONGO_DB;
-if (!MONGO_DB) {
-    throw new Error("Please ensure that the MONGO_DB env var is defined");
+const BUCKET_REGION = process.env.S3_BUCKET_REGION;
+if (!BUCKET_REGION) {
+    throw new Error("Make sure S3_BUCKET_REGION is defined in the .env file, and that it is the region the target bucket is in");
 }
 
-const FORM_STATE_COLLECTION = "formstates";
+import AWS from "aws-sdk";
+const S3 = new AWS.S3({
+    signatureVersion: "v4",
+    region: BUCKET_REGION
+});
 
 import * as Express from "express";
-import { MongoClient } from "mongodb";
+import { PromiseResult } from "aws-sdk/lib/request";
 
 export const Route = "/formstate/:id";
 export let MiddlewareFunc: Express.Handler = async function(req: Express.Request, res: Express.Response, _next: Express.NextFunction) {
@@ -30,20 +34,23 @@ export let MiddlewareFunc: Express.Handler = async function(req: Express.Request
         return;
     }
 
-    let mongoClient = new MongoClient(MONGO_URL, { useUnifiedTopology: true });
-    await mongoClient.connect();
-
-    let mongoDb = mongoClient.db(MONGO_DB);
-    let stateCollection = mongoDb.collection(FORM_STATE_COLLECTION);
-
     if (req.method == "POST") {
         let stateObject = req.body;
-        let stateDocument = {
-            id: formId,
-            state: stateObject
-        };
+        let statePayload = JSON.stringify(stateObject);
+        
+        let putResult = await S3.putObject({
+            Key: "formstates/" + formId + ".json",
+            Bucket: TARGET_BUCKET,
+            Body: statePayload
+        }).promise();
 
-        await stateCollection.insertOne(stateDocument);
+        if (putResult.$response.error) {
+            console.error("Error trying to put the form state into S3", putResult.$response.error);
+            res.status(500).send();
+
+            return;
+        }
+
         res.status(201);
         res.send();
 
@@ -51,15 +58,31 @@ export let MiddlewareFunc: Express.Handler = async function(req: Express.Request
     }
 
     if (req.method == "GET") {
-        let stateDocument = await stateCollection.findOne({ id: formId });
-        if (!stateDocument) {
-            res.status(404);
-            res.send();
+        let getResult: PromiseResult<AWS.S3.GetObjectOutput, AWS.AWSError>;
+        
+        try {
+            getResult = await S3.getObject({
+                Bucket: TARGET_BUCKET,
+                Key: "formstates/" + formId + ".json"
+            }).promise();
+
+            
+            if (getResult.$response.error) {
+                throw getResult.$response.error;
+            }
+        } catch (error) {
+            if (error.code == "NoSuchKey") {
+                res.status(404).send();
+                return;
+            }
+
+            console.error("Error trying to get the form state from S3", error);
+            res.status(500).send();
 
             return;
         }
-
-        let stateObject = stateDocument.state;
+        
+        let stateObject = JSON.parse(getResult.Body.toString());
         res.status(200);
         res.json(stateObject);
 
